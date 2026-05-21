63ae1197bcaa4e28b14357b5c71c43c34766b68b
Nishkarsh Hublikar <nishkarshhublikar@gmail.com>
2026-05-21 09:08:29 +0530

    feat: Initialize ReadmeFlix CLI with TypeScript Setup and added repository analyze command.

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+import { Command } from "commander";
+import chalk from "chalk";
+import figlet from "figlet";
+import gradientString from "gradient-string";
+
+import { analyzeProject } from "./commands/analyze.js";
+
+const program = new Command();
+
+console.log(
+  gradientString.pastel.multiline(
+    figlet.textSync("ReadmeFlix", {
+      horizontalLayout: "default",
+      verticalLayout: "default",
+    })
+  )
+);
+
+console.log(
+  chalk.gray("\nGenerate cinematic README files automatically.\n")
+);
+
+program
+  .name("readmeflix")
+  .description("AI-powered README generator")
+  .version("1.0.0");
+
+program
+  .command("analyze")
+  .description("Analyze repository")
+  .action(analyzeProject);
+
+program.parse();
